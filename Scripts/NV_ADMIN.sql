4fa046fa33009e566da72ac0a4c0b5dcc1250151
Yeah_gram <syeahc@gmail.com>
2021-12-07 14:31:22 +0900

    스프링 시큐리티 적용, 오류해결중

--- Scripts/NV_ADMIN.sql
+++ Scripts/NV_ADMIN.sql
@@ -1,4 +1,4 @@
-
+/*관리자 테이블*/
 CREATE TABLE NV_ADMIN(
 	ADMIN_ID VARCHAR(255) PRIMARY KEY,
 	ADMIN_PW VARCHAR(255) NOT NULL,
@@ -6,5 +6,12 @@ CREATE TABLE NV_ADMIN(
 	REG_DATE DATETIME DEFAULT now()
 );
 
-INSERT INTO NV_ADMIN(ADMIN_ID,ADMIN_PW,EMAIL) VALUES('admin2','admin1234','');
+/*테이블 조회*/
 SELECT * FROM nv_admin;
+
+/*-------------------------------------------------------------------------------*/
+
+INSERT INTO NV_ADMIN(ADMIN_ID,ADMIN_PW,EMAIL) VALUES('admin2','admin1234','');
+
+SELECT ADMIN_ID FROM NV_ADMIN
+        WHERE ADMIN_ID='admin2' AND ADMIN_PW='admin1234';
