5a4bce0b8d3c575840b6704a50a2186e0fd66b36
eastgermkim <eastgerm93@gmail.com>
2022-02-28 18:27:38 +0900

    supportcontroller 수신 메일 변경

--- src/main/java/com/newvisioneng/controller/SupportController.java
+++ src/main/java/com/newvisioneng/controller/SupportController.java
@@ -268,7 +268,7 @@ public class SupportController {
 		 try {
 			 
 			 //수신받을 이메일 지정 nv3000@empas.com으로 바뀔 예정
-			 dto.setToMail("eastgerm@nate.com");;
+			 dto.setToMail("nvengsmtp@gmail.com");;
 			/* System.out.println("내용:......................"+dto.getContent());*/
 			 
 			// dto(메일관련 정보)를 sendMail에 저장함
